1dca3239119417b10ebeece08efde3cf8dea41cf
Fernando Kenzo Imami Fugihara <f205067@dac.unicamp.br>
2026-03-19 18:13:09 +0000

    Workspace: added overloadedUsers and projectHealth functions

--- src/main/java/com/nexus/service/Workspace.java
+++ src/main/java/com/nexus/service/Workspace.java
@@ -1,8 +1,12 @@
 package com.nexus.service;
 
 import com.nexus.model.Task;
+import com.nexus.model.User;
+import com.nexus.model.TaskStatus;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.Collections;
 
 public class Workspace {
@@ -16,4 +20,34 @@ public class Workspace {
         // Retorna uma visão não modificável para garantir encapsulamento
         return Collections.unmodifiableList(tasks);
     }
+
+    // public List<User> topPerformers() {
+    //     // Retorna top 3 usuários com mais tarefas concluídas (STATUS == DONE)
+    // }
+
+    public List<User> overloadedUsers() {
+        // Retorna todos usuários com carga de trabalho > 10 tasks com STATUS == in PROGRESS
+        return tasks.stream()
+            .filter(t -> t.getStatus() == TaskStatus.IN_PROGRESS)
+            .map(Task::getOwner)
+            .filter(u -> u.calculateWorkload() > 10)
+            .distinct()
+            .toList();
+    }
+
+    public float projectHealth() {
+        // Retorna percentual de conclusão do projeto, ou seja, tasks DONE / total tasks
+        long doneTasks = tasks.stream()
+            .filter(t -> t.getStatus() == TaskStatus.DONE)
+            .count();
+        
+        long totalTasks = tasks.stream()
+            .count();
+        
+        return (doneTasks / totalTasks);
+    }
+
+    // public TaskStatus globalBottlenecks() {
+    //     // Verificar qual status possui maior n de tarefas no sistema - {DONE}
+    // }
 }
