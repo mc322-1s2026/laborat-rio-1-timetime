fc3f17191aa6c666925f13bce318d439a4ec636a
Fernando Kenzo Imami Fugihara <f205067@dac.unicamp.br>
2026-03-25 00:52:00 +0000

    Project javadocs

--- src/main/java/com/nexus/model/Project.java
+++ src/main/java/com/nexus/model/Project.java
@@ -3,11 +3,19 @@ import java.util.ArrayList;
 import java.util.List;
 import com.nexus.exception.NexusValidationException;
 
+/* Projeto representa um conjunto de tarefas relacionadas, com um orçamento total de horas alocadas.
+* @author Fernando Fugihara
+* @version 1.0
+*/
 public class Project {
     final private String projectName;
     private List<Task> tasks = new ArrayList<>();
     final private int totalBudget; // Horas de trabalho alocadas para o projeto
 
+    /* Construtor do Projeto.
+    * @param projectName Nome do projeto 
+    * @param totalBudget Orçamento total de horas para o projeto
+     */
     public Project(String projectName, int totalBudget) {
         if (projectName == null || projectName.trim().isBlank()) {
             throw new NexusValidationException("O nome do projeto não pode ser vazio.");
@@ -19,6 +27,11 @@ public class Project {
         this.totalBudget = totalBudget;
     }
 
+
+    /* addTask adiciona uma nova tarefa ao projeto, garantindo que o orçamento total de horas não seja excedido.
+    - @param new_task A tarefa a ser adicionada ao projeto
+    - Valida se a tarefa é nula ou se a adição da tarefa excederia o orçamento total do projeto.
+    */
     public void addTask(Task new_task) {
         if (new_task == null) {
             throw new IllegalArgumentException("Tarefa não pode ser nula.");
